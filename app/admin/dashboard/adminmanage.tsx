d6611912dd8cb74499d1385260c61c8840ae19f8
nicomari-commits <nicomaripogi@gmail.com>
2025-09-26 11:00:35 +0800

    Update adminmanage.tsx

--- app/admin/dashboard/adminmanage.tsx
+++ app/admin/dashboard/adminmanage.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
